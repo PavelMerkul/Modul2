7e681763c5ed5069e293571bd00dccd8f4c0dae9
Pavel MSK <sonev@bk.ru>
2024-07-10 23:10:10 +0300

    commit main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        double ticketPrice = 13676; // стоимость билета
+        double ticketPrice = 13_676; // стоимость билета
         int milesPerRubles = 20; // количество рублей для одной бонусной мили
         int bonusMiles = (int) (ticketPrice / milesPerRubles);// Расчитываем бонус
         System.out.println("За билет стоимостью " + ticketPrice + " руб. будет начислено " + bonusMiles + " бонусных миль");
